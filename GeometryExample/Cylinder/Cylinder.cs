158a18a7484e7afb8c6d342eaa2698d8ff70f281
Towly24 <caonamtq249@gmail.com>
2023-01-10 19:40:07 +0700

    10/1

---
+++ GeometryExample/Cylinder/Cylinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample.Cylinder
+{
+    public class Cylinder
+    {
+        private Double radius;
+        private Double height;
+        private Double BaseArea;
+        private Double LateralArea;
+        private Double TotalArea;
+        private Double Volume;
+        public Double Radius
+        {
+            get { return this.radius; }
+            set { this.radius = value; }
+        }
+        public Double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public Cylinder() { }
+
+        public Cylinder(float radius, float height) { }
+        public void Process()
+        {
+            BaseArea = radius * radius * Math.PI  ;
+            LateralArea = 2 * Math.PI * radius * height;
+            TotalArea = 2 * Math.PI * radius * (radius + height);
+            Volume = Math.PI *radius *radius * height;
+        }
+        public void Result()
+        {
+            Console.WriteLine("Cylinder Characteristics");
+            Console.WriteLine("Radius: " + radius + ",Height: " + height);
+            Console.WriteLine("Base: " + BaseArea + "- Lateral: " + LateralArea + "- Total: " + TotalArea + "- Volume: " + Volume);
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using GeometryExample.Cylinder;
+using System;
+Cylinder c = new Cylinder();
+c.Radius = 38.64;
+c.Height = 22.48;
+c.Process();
+c.Result();
+
+
